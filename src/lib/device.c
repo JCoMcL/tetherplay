740d0173753035b6bffe82cb4bf5e1ae01435080
JCoMcL <rapcyss@gmail.com>
2021-02-07 21:35:01 +0000

    rewrite device.c to use libevdev, ignore generated bindings

--- src/lib/device.c
+++ src/lib/device.c
@@ -1,119 +1,87 @@
-#include <libevdev/libevdev.h>
+#include <libevdev-1.0/libevdev/libevdev.h> // this sucks
+#include <libevdev-1.0/libevdev/libevdev-uinput.h> // this sucks
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #include "device.h"
 
-int fd;
-
-struct input_event create_input_event(int type, int code, int val){
-   return (struct input_event) {{0}, type, code, val};
+void enable_event(struct libevdev *dev, int type, int event_code){
+	libevdev_enable_event_type(dev, type); // only required once per event type
+	libevdev_enable_event_code(dev, type, event_code, NULL);
 }
 
-struct input_event create_key_event(int code, int val){
-   return create_input_event(EV_KEY, code, val);
+void enable_key_event(struct libevdev *dev, int event_code){
+	enable_event(dev, EV_KEY, event_code);
 }
 
-void write_input_event(struct input_event ie) {
-   write(fd, &ie, sizeof(ie));
+void enable_abs_event(struct libevdev *dev, int event_code){
+	enable_event(dev, EV_ABS, event_code);
 }
 
-// execute buffered events
-void sync_events(){
-   write_input_event( create_input_event(EV_SYN, SYN_REPORT, 0) );
+void cleanup(struct libevdev_uinput *dev){
+	libevdev_uinput_destroy(dev);
 }
 
-void press(int code){
-   write_input_event( create_key_event(code, 1) );
-   sync_events();
+void hardcode_device(struct libevdev *dev) {
+	enable_abs_event(dev, ABS_X);
+	enable_abs_event(dev, ABS_Y);
+	enable_key_event(dev, BTN_SOUTH);
+	enable_key_event(dev, BTN_WEST);
+	enable_key_event(dev, BTN_START);
 }
 
-void release(int code){
-   write_input_event( create_key_event(code, 0) );
-   sync_events();
+struct libevdev_uinput *create_device(char *name){
+	int err;
+
+	struct libevdev *dev;
+
+	dev = libevdev_new();
+	libevdev_set_name(dev, "test device");
+	hardcode_device(dev);
+
+	struct libevdev_uinput *uidev;
+	err = libevdev_uinput_create_from_device( dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
+	if (err != 0) {}
+
+	return uidev;
 }
 
-void click(int code){
-   press(code);
-   release(code);
+void write_key_event(struct libevdev_uinput *dev, int code, int value){
+	libevdev_uinput_write_event(dev, EV_KEY, code, value);
 }
 
-void move_joystick(int code, int pos){
-   write_input_event( create_key_event(code, pos) );
+void sync_events(struct libevdev_uinput *dev){
+	libevdev_uinput_write_event(dev, EV_SYN, SYN_REPORT, 0);
 }
 
-void enable_event(int file_descriptor, int type, int event_code){
-   ioctl(file_descriptor, type, event_code);
+void press(struct libevdev_uinput *dev, int code){
+	write_key_event(dev, code, 1);
+	sync_events(dev);
 }
 
-void enable_key_event(int event_code){
-   enable_event(fd, UI_SET_KEYBIT, event_code);
+void release(struct libevdev_uinput *dev, int code){
+	write_key_event(dev, code, 0);
+	sync_events(dev);
 }
 
-void enable_abs_event(int event_code){
-   enable_event(fd, UI_SET_ABSBIT, event_code);
+void click(struct libevdev_uinput *dev, int code){
+	press(dev, code);
+	release(dev, code);
 }
 
-void setup_dpad_events(){
-   enable_key_event(BTN_DPAD_UP);
-   enable_key_event(BTN_DPAD_LEFT);
-   enable_key_event(BTN_DPAD_DOWN);
-   enable_key_event(BTN_DPAD_RIGHT);
+void move_joystick(struct libevdev_uinput *dev, int code, int pos){
+	write_key_event(dev, code, pos);
 }
 
-void setup_gamepad_south(){
-   enable_key_event(BTN_SOUTH);
-}
-void setup_gamepad_east(){
-   enable_key_event(BTN_EAST);
-}
-void setup_gamepad_west(){
-   enable_key_event(BTN_WEST);
-}
-void setup_gamepad_north(){
-   enable_key_event(BTN_NORTH);
-}
+#ifndef PRIVATE
+int main() {
+	struct libevdev_uinput *dev = create_device("test");
 
-// enable start and select buttons
-void setup_menupad_events(){
-   ioctl(fd, UI_SET_KEYBIT, BTN_START);
-   ioctl(fd, UI_SET_KEYBIT, BTN_SELECT);
+	press(dev, BTN_SOUTH);
+	sleep(1);
+	release(dev, BTN_SOUTH);
+	sleep(1);
+	return 0;
 }
-
-void setup_joystick_left_events(){
-   ioctl(fd, UI_SET_KEYBIT, ABS_X);
-   ioctl(fd, UI_SET_KEYBIT, ABS_Y);
-}
-
-void setup_joystick_right_events(){
-   ioctl(fd, UI_SET_KEYBIT, ABS_RX);
-   ioctl(fd, UI_SET_KEYBIT, ABS_RY);
-}
-
-void create_device(){
-   struct uinput_setup usetup;
-   memset(&usetup, 0, sizeof(usetup));
-   usetup.id.bustype = BUS_USB;
-   usetup.id.vendor = 0x1234;
-   usetup.id.product = 0x5678;
-   strcpy(usetup.name, "Test");
-
-   ioctl(fd, UI_DEV_SETUP, &usetup);
-   ioctl(fd, UI_DEV_CREATE);
-}
-
-void destroy_device(){
-   ioctl(fd, UI_DEV_DESTROY);
-   close(fd);
-}
-
-// open uinput at provided path and set global file-descriptor
-void open_path(char* path){
-   fd = open(path, O_WRONLY | O_NONBLOCK);
-   ioctl(fd, UI_SET_EVBIT, EV_KEY);
-}
-
-void open_default(){
-   open_path("/dev/uinput");
-}
+#endif
--- src/lib/device.h
+++
@@ -1,4 +0,0 @@
-/* This file was automatically generated.  Do not edit! */
-struct input_event create_key_event(int code,int val);
-struct input_event create_input_event(int type,int code,int val);
-extern int fd;

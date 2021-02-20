20fa56ca3f23a17d22430c322c1f260e9c5b55eb
Niall Bermingham <niallbermingham4@mail.dcu.ie>
2021-02-20 00:06:14 +0000

    joystick recognised

--- src/device.c
+++ src/device.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <linux/uinput.h>
 
 #include "device.h"
 
@@ -20,19 +21,17 @@ static void enable_key_event(struct libevdev *dev, int event_code){
 }
 
 static void enable_abs_event(struct libevdev *dev, int event_code){
-	libevdev_enable_event_type(dev, EV_ABS);
-	libevdev_enable_event_code(dev, EV_ABS, event_code, NULL);
-	struct input_absinfo abs;
-	abs.value = 512;
-	abs.flat = 0;
-	abs.fuzz = 0;
-	abs.resolution = 0;
-	abs.maximum = 512;
-	abs.minimum = -512;
-	libevdev_set_abs_info(dev, event_code, &abs);
+	struct input_absinfo *abs = malloc(sizeof(struct input_absinfo));
+	abs->maximum=512;
+	abs->minimum=-512;
+	abs->flat=15;
+	libevdev_enable_event_code(dev, EV_ABS, event_code, &abs);
+	free(abs);
 }
 
 static void hardcode_device(struct libevdev *dev) {
+	libevdev_enable_event_type(dev, EV_ABS);
+
 	enable_abs_event(dev, ABS_X);
 	enable_abs_event(dev, ABS_Y);
 	enable_key_event(dev, BTN_SOUTH);
@@ -40,17 +39,19 @@ static void hardcode_device(struct libevdev *dev) {
 	enable_key_event(dev, BTN_START);
 }
 
+
 void create_device(char *name){
 	int err;
 
 	struct libevdev *dev;
-
+	memset(&dev, 0, sizeof(dev));
 	dev = libevdev_new();
 	hardcode_device(dev);
 	libevdev_set_name(dev, name);
 
 	err = libevdev_uinput_create_from_device( dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
 	if (err != 0) {}
+
 
 }
 
@@ -81,7 +82,7 @@ void click( int code){
 }
 
 void move_abs_event( int code, int pos){
-	write_abs_event(code, pos);
+	libevdev_uinput_write_event(uidev, EV_ABS, code, pos);
 	sync_events();
 }
 

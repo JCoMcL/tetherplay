59181cc38acd623ad10a82d24d53d985c1d2f9d7
JCoMcL <rapcyss@gmail.com>
2021-02-28 19:54:56 +0000

    make error macros less error-prone

--- src/errutil.h
+++ src/errutil.h
@@ -12,5 +12,9 @@
 
 #define RETURN_IF_FALSE(expr, error_num) RETURN_IF_TRUE(!(expr), error_num)
 
-#define RETURN_IF_ERROR(expr, error_num) RETURN_IF_FALSE((expr) == 0, error_num)
-#define RETURN_IF_ERRNO(expr) int result = expr; RETURN_IF_ERROR(result, result > 0 ? -result : result)
+#define RETURN_IF_ERROR(expr, error_num) RETURN_IF_TRUE((expr) < 0, error_num)
+
+//stupid hack to assign variable in macros without breaking everything
+#define SUBSCOPE(assignment, expression) for(assignment;;) {expression; break;}
+
+#define RETURN_IF_ERRNO(expr) SUBSCOPE(int result = -abs(expr), RETURN_IF_ERROR(result, result))

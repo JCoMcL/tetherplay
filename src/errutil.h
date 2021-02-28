19f418af6e2188389ee46be4613c7978b18f7444
JCoMcL <rapcyss@gmail.com>
2021-02-28 12:49:43 +0000

    fix click, improve error handling and messages

--- src/errutil.h
+++ src/errutil.h
@@ -7,6 +7,7 @@
 
 #define error(status, msg) error(status, errno, RED(msg))
 #define RETURN_IF_FALSE(expr, error_num) if(!expr) {errno = error_num; return -1;}
+#define RETURN_IF_TRUE(expr, error_num) RETURN_IF_FALSE(!expr, error_num)
 
 #define RETURN_IF_ERROR(expr, error_num) RETURN_IF_FALSE(expr == 0, error_num)
 #define RETURN_IF_ERRNO(expr) RETURN_IF_ERROR(expr, expr > 0 ? -expr : expr)

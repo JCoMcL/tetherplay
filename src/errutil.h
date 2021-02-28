0b8a481b8f575b62fe84e1da4933e8c996e91ebf
JCoMcL <rapcyss@gmail.com>
2021-02-28 16:21:46 +0000

    fix RETURN_IF_FALSE not working due to operator precedence

--- src/errutil.h
+++ src/errutil.h
@@ -10,7 +10,7 @@
 
 #define RETURN_IF_TRUE(expr, error_num) if(expr) {errno = error_num; return -error_num;}
 
-#define RETURN_IF_FALSE(expr, error_num) RETURN_IF_TRUE(!expr, error_num)
+#define RETURN_IF_FALSE(expr, error_num) RETURN_IF_TRUE(!(expr), error_num)
 
-#define RETURN_IF_ERROR(expr, error_num) RETURN_IF_FALSE(expr == 0, error_num)
+#define RETURN_IF_ERROR(expr, error_num) RETURN_IF_FALSE((expr) == 0, error_num)
 #define RETURN_IF_ERRNO(expr) int result = expr; RETURN_IF_ERROR(result, result > 0 ? -result : result)

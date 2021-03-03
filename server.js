49c1500121caf2813409830f6f05d7e7e5192cda
Niall Bermingham <niallbermingham4@mail.dcu.ie>
2021-03-03 22:05:56 +0000

    rebase conflicts

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-<<<<<<< HEAD
 const getopts = require('getopts')
 const ws = require('./ws')
 
@@ -10,12 +9,8 @@ const config = getopts(process.argv.slice(2), {
 })
 
 ws.start(config)
-=======
-require('./ws')
-require('./style/style.js')
 var qrcode = require("qrcode-terminal")
 var ip = require("ip")
->>>>>>> 3b07659 (add qrcode, rebased multi-user, change config for 4 gp buttons)
 var app = express()
 
 var port = 3000
@@ -27,20 +22,11 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-<<<<<<< HEAD
 app.get('/favicon.png', function (req, res) {
     res.sendFile(__dirname + '/img/tetherplay.png');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-=======
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
-<<<<<<< HEAD
-  qrcode.generate(`https://${ip.address()}:${port}`);
->>>>>>> 3b07659 (add qrcode, rebased multi-user, change config for 4 gp buttons)
-=======
   qrcode.generate(`http://${ip.address()}:${port}`);
->>>>>>> 31fa8a6 (change https to http for qr code)
 })

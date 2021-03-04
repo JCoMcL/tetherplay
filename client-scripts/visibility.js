0b373913d31b4ac04eef4554e52ef1f1dce380f8
Niall Bermingham <niallbermingham4@mail.dcu.ie>
2021-03-04 13:13:38 +0000

    refactor toggleVisibility, tidy up stylesheet

--- client-scripts/visibility.js
+++ client-scripts/visibility.js
@@ -1,29 +1,17 @@
 function toggleIDVisibility(id){
     var obj = document.getElementById(id);
     var classes = obj.classList;
-    if (classes.contains('hidden')){
-        classes.remove('hidden');
-    } else {
-        classes.add('hidden');
-    }
+    classes.toggle('hidden');
+    
 }
 function toggleClassVisibility(classname){
-    var elem = document.getElementsByClassName(classname);
-    for (i=0;i<elem.length; i++ ){
-        if (elem[i].classList.contains('hidden')){
-            elem[i].classList.remove('hidden');
-        } else {
-            elem[i].classList.add('hidden');
-        }
-    }
+    var elem = document.getElementsByClassName(classname),
+        clsslst = elem.classList;
+    clsslst.toggle('hidden');
 }
 function toggleParentIDVisibility(id){
     var obj = parent.document.getElementById(id);
     var classes = obj.classList;
-    if (classes.contains('hidden')){
-        classes.remove('hidden');
-    } else {
-        classes.add('hidden');
-    }
+    classes.toggle('hidden'); 
 }
 

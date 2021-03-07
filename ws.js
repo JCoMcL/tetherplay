f41fb24627c3b5a5370d7d270d0c5450a1f17a33
JCoMcL <rapcyss@gmail.com>
2021-03-07 13:11:42 +0000

    handle clinet or controller unexpectedly disconnecting

--- ws.js
+++ ws.js
@@ -2,12 +2,30 @@ function start(config) {
 	const wss = new (require('ws').Server)({port: 40510})
 	const tpinput = require('./tpinput')
 
+	function bind_controller(websocket) {
+		websocket.controller = tpinput.open(config)
+		websocket.controller.on('close', (code, sig) => {
+			if (!sig) {
+				console.error(`${ws.id}: Controller closed unexpectedly, restarting`)
+				bind_controller(websocket)
+			}
+		})
+	}
+
 	wss.on('connection', (ws, req) => {
-		console.log(`new connection: ${req.socket.remoteAddress}`)
-		ws.controller = tpinput.open(config)
-		ws.controller.on('close', (code, sig) => ws.close(1011) )
+		ws.id = req.socket.remoteAddress
+		console.log(`new connection: ${ws.id}`)
+
+		bind_controller(ws)
+
 		ws.on('message', message => {
 			ws.controller.stdin.write(message + '\n')
+		})
+		ws.on('close', (code, reason) => {
+			if (!reason)
+				reason = "unknown reason"
+			console.error(`${ws.id}: Connection closed: ${reason}`)
+			ws.controller.kill()
 		})
 	})
 }

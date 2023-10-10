40c5017ad0c5eb1a174dfc36feafda28552e8700
JCoMcL <rapcyss@gmail.com>
2023-10-10 14:50:17 +0100

    remove some unneeded dependencies

--- tetherplay-fhs.nix
+++ tetherplay-fhs.nix
@@ -4,12 +4,8 @@
   name = "tetherplay-fhs-env";
   targetPkgs = pkgs: with pkgs; [ 
     udev
-    libudev
     libevdev
     pkgconfig
-    rust-bindgen
-    rustfmt
-    #makeheaders not in nix yet
   ];
   profile = '' export SHELL=zsh '';
   extraOutputsToInstall = [ "dev" ];
